ca98ac69256e9473dbecbe8e041a89ed9d49799b
wwang <wwang@talend.com>
2023-09-14 06:36:47 +0300

    fix(TDI-50138): support old parquet files
    * allow read old parquet file which is maked by old api with old avro version which allow wrong default value in schema

--- parquet-avro/src/main/java/org/apache/parquet/avro/AvroWriteSupport.java
+++ parquet-avro/src/main/java/org/apache/parquet/avro/AvroWriteSupport.java
@@ -126,7 +126,7 @@ public class AvroWriteSupport<T> extends WriteSupport<T> {
   @Override
   public WriteContext init(Configuration configuration) {
     if (rootAvroSchema == null) {
-      this.rootAvroSchema = new Schema.Parser().parse(configuration.get(AVRO_SCHEMA));
+      this.rootAvroSchema = new Schema.Parser().setValidateDefaults(false).parse(configuration.get(AVRO_SCHEMA));
       this.rootSchema = new AvroSchemaConverter(configuration).convert(rootAvroSchema);
     }
 
